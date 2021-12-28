4ce5313c8a9b9386b2dfd79e043326b53470bbb8
sudipnext <parajulisudip123@outlook.com>
2021-12-28 17:41:33 +0545

    getting userinput

---
+++ getting_user_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter two Numbers:- ";
+    cin >> a >> b;
+    cout << "The sum of two numbers is:- "<< a+b;
+    return 0;
+}
