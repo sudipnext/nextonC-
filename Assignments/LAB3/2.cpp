079a9315bf8d24b23add6977893b1d93de84d5ad
sudipnext <parajulisudip123@outlook.com>
2022-02-11 14:36:14 +0545

    completed

--- Assignments/LAB3/2.cpp
+++ Assignments/LAB3/2.cpp
@@ -1,3 +1,7 @@
+// Assume that you want to check whether the number is prime or not. 
+// Write a program that asks for numbers repeatedly. When it finishes checking
+//  a number the program asks if the user wants to do another calculation. The response 
+// can be 'y' or 'n'. Don't forget to use the object-oriented concept.
 #include <iostream>
 using namespace std;
 
