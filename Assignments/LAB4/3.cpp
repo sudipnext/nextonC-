a58f32f706dadf64c94a672b42bca704345a2eb3
sudipnext <parajulisudip123@outlook.com>
2022-02-19 13:55:56 +0545

    finished

--- Assignments/LAB4/3.cpp
+++ Assignments/LAB4/3.cpp
@@ -1,1 +1,31 @@
-// Write a class that can store Department ID and Department Name with constructors to initialize its members. Write destructor member in the same class and display the message "Object n goes out of the scope". Your program should be made such that it should show the order of constructor and destructor invocation.
+// Write a class that can store Department ID and Department Name with constructors to initialize its members.
+// Write destructor member in the same class and display the message "Object n goes out of the scope".
+//  Your program should be made such that it should show the order of constructor and destructor invocation.
+#include <iostream>
+#include<string.h>
+using namespace std;
+class Department
+{
+private:
+    int deptId;
+    char deptName[50];
+
+public:
+    Department(){
+        cout << "Constructor here"<<endl;
+        deptId = 12;
+        char deptNae[] = "Pratik Department Store";
+        strcpy(deptName, deptNae);
+    }
+    ~Department(){
+        cout << "Object n goes out of the scope"<<endl;
+    }
+    char showData(){
+        cout << deptName<<endl;
+    }
+};
+int main()
+{
+    Department obj;
+    obj.showData();
+}
